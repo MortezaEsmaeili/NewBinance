9ce0861cc51ecf301276f335ea6204e24b0992fa
MortezaEsmaeili <Morteza.007@gmail.com>
2023-09-23 19:24:02 +0330

    Error Commit

---
+++ Entities/TradingData.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Entities
+{
+    public class TradingData
+    {
+        
+        public long Id { get; set; }
+        public string CoinName { get; set; }
+        public DateTime MyProperty { get; set; }
+        public string Position { get; set; }
+        public int Leverage { get; set; }
+        public int Amount { get; set; }
+        public decimal OpenPrice { get; set; }
+        public decimal ClosePrice { get; set; }
+        public decimal PositionValue { get; set; }
+        public decimal PositionMargin { get; set; }
+
+    }
+}
